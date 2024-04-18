5c03e90c1ad604a7b5b630ccf62440bc190dd15d
Franznic0 <franznicofranz@gmail.com>
2024-04-18 10:54:26 +0100

    Mobile layout adjustment

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let nNotification = document.getElementById('notification-number');
+const notifications = document.querySelectorAll('.notification-box');
+
+function notiNumAdj() {
+    let count = 0;
+
+}
